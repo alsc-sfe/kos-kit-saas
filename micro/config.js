3ba44820357034670d31fb97c16c53d5b33d4300
孤豹 <yongbao.fyb@koubei.com>
2019-07-21 23:21:49 +0800

    fix: edit to prod server

--- micro/config.js
+++ micro/config.js
@@ -1,6 +1,13 @@
 module.exports = {
   DEF_SERVER: {
-    daily: 'http://micro-daily.alibaba.net',
-    prod: 'http://micro-daily.alibaba.net',
+    daily: 'http://kbmicro.alibaba-inc.com',
+    prod: 'http://kbmicro.alibaba-inc.com',
   },
 }
+
+// module.exports = {
+//   DEF_SERVER: {
+//     daily: 'http://local.alipay.net:7001',
+//     prod: 'http://local.alipay.net:7001',
+//   },
+// }
